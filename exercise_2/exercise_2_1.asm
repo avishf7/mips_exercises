59ee79c7ea68e07b11a08e0042894a78143f7851
avishf7 <62483176+avishf7@users.noreply.github.com>
2020-09-02 14:47:39 +0300

    תרגיל 3 שאלה 1

--- exercise_2/exercise_2_1.asm
+++ exercise_2/exercise_2_1.asm
@@ -4,19 +4,20 @@ str: .ASCIIZ"The biggest number : "
 .text
 la $a0 list
 li $a1 5
-lb $t8 0($a0)
+lb $v0 0($a0)
 
 loop:
 subi $a1 $a1 1
 beq $a1 $zero end
 addi $a0 $a0 1
 lb $t0 0($a0)
-slt $t1 $t0 $t8 
+slt $t1 $t0 $v0 
 bne $t1 $zero next
-add $t8 $zero $t0
+add $v0 $zero $t0
 next:
 j loop
 end:
+move $t8 $v0
 la $a0 str
 li $v0 4
 syscall
---
+++ exercise_3/exercise_3_1.asm
@@ -0,0 +1,46 @@
+.data
+list:  5 3 10 7 4
+str: .ASCIIZ"Sorted list: " 
+blank: .ASCIIZ" " 
+.text
+la $a0 list
+li $a1 5
+
+move $t7 $a1
+move $t8 $t7
+loop:
+subi $t8 $t8 1
+beq $t8 $zero endLoop
+lw $t0 0($a0)
+lw $t1 4($a0)
+slt $t2 $t1 $t0 
+beq $t2 $zero next
+sw $t1 0($a0)
+sw $t0 4($a0)
+next:
+addi $a0 $a0 4
+j loop
+endLoop:
+la $a0 list
+subi $t7 $t7 1
+move $t8 $t7
+bne $t8 $zero loop
+
+la $a0 str
+li $v0 4
+syscall
+
+la $a2 list
+printLoop:
+beq $a1 $zero endProgram
+lw $a0 0($a2) 
+li $v0 1
+syscall
+la $a0 blank
+li $v0 4
+syscall
+
+addi $a2 $a2 4
+subi $a1 $a1 1
+j printLoop
+endProgram:
