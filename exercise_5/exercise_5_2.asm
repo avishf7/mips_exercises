59be815dfb641762fa355b02310a654282724638
avishay farkash <62483176+avishf7@users.noreply.github.com>
2020-09-06 11:58:29 +0300

    Update exercise_5_2.asm

--- exercise_5/exercise_5_2.asm
+++ exercise_5/exercise_5_2.asm
@@ -1,19 +1,19 @@
 .text
 
 li $s0 0x31
-li $s1 0x30
-li $s2 0x32
-li $s3 0x33
+li $s1 0x32
+li $s2 0x33
+li $s3 0x34
 
 li $v0 5
 syscall
 
 sll $t0 $v0 24
 
- Beq $t0 $s0 case1
+Beq $t0 $s0 case1
 Beq $t0 $s1 case2
 Beq $t0 $s2 case3
-Beq $t0 $s3 case 4
+Beq $t0 $s3 case4
 
 case1:
 case2:
